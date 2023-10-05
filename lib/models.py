db47dda1f59f2f99e0b4e1b31d3eab981e46528e
tomutanyi <tomutanyi@gmail.com>
2023-10-05 15:46:43 +0300

    this is a stable seed.py & models.py

---
+++ lib/models.py
@@ -0,0 +1,54 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+engine = create_engine('sqlite:///lightning.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+car_client = Table(
+    'car_client',
+    Base.metadata,
+    Column('client_id', ForeignKey('clients.id'), primary_key=True),
+    Column('car_id', ForeignKey('cars.id'), primary_key=True),
+    extend_existing=True,
+)
+
+class Car(Base):
+    __tablename__ = "cars"
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(), index=True)
+    price = Column(Integer())
+
+    clients = relationship('Client', secondary=car_client, back_populates='cars')
+    feedbacks = relationship("Feedback", back_populates="car")
+
+    def __repr__(self):
+        return f"Car name: {self.name}, price: {self.price}"
+
+class Client(Base):
+    __tablename__ = "clients"
+
+    id = Column(Integer(), primary_key=True)
+    first_name = Column(String(), index=True)
+    last_name = Column(String())
+
+    feedbacks = relationship("Feedback", back_populates="client")
+    cars = relationship('Car', secondary=car_client, back_populates='clients')
+
+    def __repr__(self):
+        return f"Client's first name: {self.first_name}, last name: {self.last_name}"
+
+
+class Feedback(Base):
+    __tablename__ = "feedbacks"
+
+    id = Column(Integer(), primary_key=True)
+    star_ratings = Column(Integer())
+    comment = Column(String(length=40))  # Limit feedback to a maximum of 40 characters
+
+    car_id = Column(Integer(), ForeignKey('cars.id'))
+    client_id = Column(Integer(), ForeignKey('clients.id'))
+
